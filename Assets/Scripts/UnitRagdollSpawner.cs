d07ccb3c624e1a4e5ad54f32061592ce39bfe3e1
Zachary Dowd <zacharyd2796@zohomail.com>
2022-06-29 11:23:40 -0600

    unit ragdoll added, next unit ui

--- Assets/Scripts/UnitRagdollSpawner.cs
+++ Assets/Scripts/UnitRagdollSpawner.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitRagdollSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform ragdollPrefab;
+    [SerializeField] private Transform originalRootBone;
+    private HealthSystem healthSystem;
+    private void Awake()
     {
-        
+        healthSystem = GetComponent<HealthSystem>();
+
+        healthSystem.OnDeath += HealthSystem_OnDeath;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HealthSystem_OnDeath(object sender, EventArgs e)
     {
-        
+        Transform ragdollTransform = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+        UnitRagdoll unitRagdoll = ragdollTransform.GetComponent<UnitRagdoll>();
+        unitRagdoll.Setup(originalRootBone);
     }
 }
