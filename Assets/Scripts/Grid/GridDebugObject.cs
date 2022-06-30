37e642fc80a2bf558ec6bd4fb7688b06fe5e685b
Zachary Dowd <zacharyd2796@zohomail.com>
2022-06-30 15:12:12 -0600

    pathfinding debug object created

--- Assets/Scripts/Grid/GridDebugObject.cs
+++ Assets/Scripts/Grid/GridDebugObject.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class GridDebugObject : MonoBehaviour
 {
     [SerializeField] private TextMeshPro tmp;
-    private GridObject gridObject;
-    public void SetGridObject(GridObject gridObject)
+    private object gridObject;
+    public virtual void SetGridObject(object gridObject)
     {
         this.gridObject = gridObject;
     }
 
-    private void Update()
+    protected virtual void Update()
     {
         tmp.text = gridObject.ToString();
     }
